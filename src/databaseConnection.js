2be28a68cb638926c4eeb3a41f5c4a6cbbeac29b
LikhithPalya <145858684+LikhithPalya@users.noreply.github.com>
2024-10-06 23:16:57 +0530

    Update databaseConnection.js

--- src/databaseConnection.js
+++ src/databaseConnection.js
@@ -5,7 +5,7 @@ dotenv.config({path: './.env'})
 
 const connectDB = async()=>{
     try {
-        const connectionInstance = await mongoose.connect(`$process.env.MONGODB_URI`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log(`\n MongoDB connection successful DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log(`Error occured while connecting to DB ${error}`)
